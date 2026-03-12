ccaa4791015c8595a1049cf645b48c6f5581cbfd
komop9988-collab <komop9988@gmail.com>
2026-03-13 01:15:37 +0700

    Add files via upload
    Upload Django form assignment 8

---
+++ myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def form(request):
+    return render(request, 'form.html')
+# Create your views here.
